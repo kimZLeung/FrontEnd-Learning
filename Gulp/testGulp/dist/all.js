1c6af99c0cf6b075b759d06fd2024eb6b0d9e3ce
kimzliang <740872230@qq.com>
2016-12-14 01:13:56 +0800

    gulp

---
+++ Gulp/testGulp/dist/all.js
@@ -0,0 +1,5 @@
+var btn = document.getElementById('haha');
+btn.onclick = function(e) {
+	console.log('bbbbbibibibbi');
+}
+console.log('haahahahaha');
---
+++ Gulp/testGulp/js/a.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById('haha');
+btn.onclick = function(e) {
+	console.log('bbbbbibibibbi');
+}
+
+var index = require('./index.js')
