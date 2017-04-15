53b3916b7ece0904a0ecfd30aa37f001f8064665
kimzliang <740872230@qq.com>
2017-04-15 14:18:51 +0800

    node webServer router

---
+++ Node/deepInLightOutNode/webServer/router.js
@@ -0,0 +1,66 @@
+var routerHandler = function() {
+	var router = []
+	var _use = function(path, action) {
+		router.push([path, action])
+	}
+	var _deepUse = function(setPath, path, action) {
+		for(var i = 0; i < router.length; i++) {
+			var route = router[i]
+			if(route[0] == setPath) {
+				var array = route[3]
+				if(array) {
+					route[3].push([path, action])
+				} else {
+					route[3] = []
+					route[3].push([path, action])
+				}
+			}
+		}
+		
+	}
+
+	return {
+		use: function(path, action) {
+			_use(path, action)
+		},
+		deepUse: function(setPath, path, action) {
+			_deepUse(setPath, path, action)
+		},
+		deepScan: function(frontPath, path, req, res) {
+			for(var i = 0; i < frontPath.length; i++) {
+				var route = frontPath[i]
+				if(path === route[0]) {
+					var action = route[1]
+					action(req, res)
+					return
+				}
+			}
+		},
+		getChildRoute: function(pathName) {
+			var pathArray = pathName.split('/')
+			var setArray = pathArray.slice(2)
+			return setArray.join('/')
+		},
+		scanRoute: function(pathName, req, res) {
+			for(var i = 0; i < router.length; i++) {
+				var route = router[i]
+				if(pathName === route[0]) {
+					var action = route[1]
+					action(req, res)
+					return
+				}
+				if(pathName.indexOf(route[0]) !== -1) {
+					var action = route[1]
+					action(req, res)
+					var setPath = this.getChildRoute(pathName)
+					if(route[3]) {
+						this.deepScan(route[3], setPath, req, res)
+					}
+				}
+			}
+		},
+		// TODO
+	}
+}()
+
+module.exports.routerHandler = routerHandler
---
+++ Node/deepInLightOutNode/webServer/server.js
@@ -0,0 +1,36 @@
+var http = require('http')
+var routerHandler = require('./router.js').routerHandler
+var url = require('url')
+
+routerHandler.use('/haha', function(req, res) {
+	res.write('haha')
+})
+
+routerHandler.deepUse('/haha', 'haha', function(req, res) {
+	res.write('haha')
+})
+
+http.createServer(function (req, res) {
+
+	var pathName = url.parse(req.url).pathname
+
+	if(req.method === 'POST') {
+		var buffers = []
+		req.on('data', function (data) {
+			buffers.push(data)
+		}).on('end', function () {
+			var buffer = Buffer.concat(buffers)
+			var dataBody = buffer.toString('utf-8')
+			res.writeHead(200)
+			routerHandler.scanRoute(pathName, req, res)
+			res.end('HW')
+		})
+	} else {
+		res.writeHead(200)
+		routerHandler.scanRoute(pathName, req, res)
+		res.end('HW')
+	}
+	
+}).listen(8080, function () {
+	console.log('server is listening on port 8080')
+})
