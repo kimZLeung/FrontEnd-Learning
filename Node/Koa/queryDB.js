10ec7dac292ae9c1ed53dea03915233e577eacb6
kimzliang <740872230@qq.com>
2017-08-07 20:03:20 +0800

    feat: 依赖注入

--- Node/Koa/queryDB.js
+++ Node/Koa/queryDB.js
@@ -1,2 +1,33 @@
 const mysql = require('mysql')
 
+// 创建数据库连接池
+const pool = mysql.createPool({
+  host     :  'XXX',
+  user     :  'XXX',
+  password :  'XXX',
+  database :  'XXX'
+})
+
+// 封装数据库查询接口
+let query = function(sql, values) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        reject( err )
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          // 释放该连接
+          connection.release()
+        })
+      }
+    })
+  })
+}
+
+// 暴露数据库查询接口
+module.exports = query
