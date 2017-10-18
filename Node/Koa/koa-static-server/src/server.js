f2e50e78813710da48bc809e546100b8f1e692cc
kimzliang <740872230@qq.com>
2017-10-18 19:45:18 +0800

    fix: 修改对mime类型的判断条件

--- Node/Koa/koa-static-server/src/server.js
+++ Node/Koa/koa-static-server/src/server.js
@@ -26,8 +26,16 @@ app.use(async (ctx) => {
 		ctx.type = _mime
 	}
 
-	if ( _mime && _mime.indexOf('image/') >= 0 || _mime.indexOf('video/') >= 0 ) {
-		// 如果是图片，则用node原生res，输出二进制数据
+	/**
+	 * 这里要注意判断，有两个逻辑判断运算符。JS逻辑运算符的短路规则是
+	 * && 前面返回false的话，直接返回前面的东西，不执行后面的代码
+	 * || 前面返回true的话，直接返回前面的东西，不执行后面的代码
+	 * 一开始利用短路的方式去判断并使用_mime的indexOf方法是可行的
+	 * 但是由于多了一个视频的判断，我们在_mime为空的时候，依然会执行到后面的indexOf
+	 * 即后面部分不会被短路，所以我们需要在逻辑或（||）后面多判断一次_mime
+	 */
+	if ( _mime && _mime.indexOf('image/') >= 0 || _mime && _mime.indexOf('video/') >= 0 ) {
+		// 如果是图片，视频，则用node原生res，输出二进制数据
 		ctx.res.writeHead(200)
 		ctx.res.write(_content, 'binary')
 		ctx.res.end()
