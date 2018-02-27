b4c3e58d4b007653d62360c9c4c21d595e85bc8d
kimzliang <740872230@qq.com>
2018-02-27 22:21:58 +0800

    change

--- 试一下/haha/kim.js
+++ 试一下/haha/kim.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var name = process.argv[2]
 var shell = require("shelljs")
+var git = require("simple-git")()
 
 switch (name) {
 	case 'new': 
@@ -10,6 +11,11 @@ switch (name) {
 		shell.mkdir('dist')
 		shell.cd('dist/')
 		shell.exec('touch dist.js')
+		break
+	case 'git': 
+		git.add('.').commit(process.argv[3]).push(['origin', 'master'], function () {
+			shell.echo('commit:', process.argv[3], 'success push to branch master')
+		})
 		break
 	default:
 		shell.echo("hello " + name)
