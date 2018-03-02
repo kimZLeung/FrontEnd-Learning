7458e80e70aa3a12bd80353e106c0c5bb7a4d3c3
kimzliang <740872230@qq.com>
2018-03-03 00:30:46 +0800

    fix: 尝试了一下curry，失败了

--- 函数式编程/curry.js
+++ 函数式编程/curry.js
@@ -23,6 +23,22 @@ function toArray(arr) {
 }
 
 
+// function _curry(fn) {
+// 	var len = fn.length
+// 	var param = []
+// 	return function _subCurry() {
+
+// 		console.log(len, param.length, arguments.length)
+// 		if (arguments.length < len - param.length) {
+// 			param = param.concat(Array.prototype.slice.call(arguments))
+// 			return _subCurry
+// 		} else {
+// 			param = param.concat(Array.prototype.slice.call(arguments))
+// 			fn.apply(this, param)
+// 		}
+// 	}
+// }
+
 // test
 function add(a, b, c) {
 	console.log('result', a + b + c)
